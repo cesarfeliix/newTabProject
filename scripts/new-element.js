2b23d25770379f9e817e1d361934faf63c5c55d8
cesarfeliix <feliixcesar@gmail.com>
2022-01-18 10:29:32 -0300

    done

--- scripts/new-element.js
+++ scripts/new-element.js
@@ -1,5 +1,30 @@
-$(document).ready(function () {
-    //console.log("new-element.js is working")
-    $("#linksContainer")
-    .append($("<li>").text("Some Text."));
-});
+var defaults = {
+    // CSS selectors and attributes that would be used by the JavaScript functions
+    favItemName: "fav-item-name",
+    favItemLink: "fav-item-link",
+    taskId: "task-",
+    formId: "todo-form",
+    dataAttribute: "data",
+    deleteItem: "delete-item"
+  }
+
+// Add Task
+//Guys don't forget, we are nothing but pencils in the hands
+//of the feeling.
+var generateElement = function(params) {
+    var parent = $(codes[params.code]),
+        wrapper;
+    if (!parent) {
+      return;
+    }
+    wrapper = $("#favLinks", {
+      "class" : defaults.favItemName,
+      "id" : defaults.taskId + params.id,
+      "data" : params.id
+    }).appendTo(parent);
+    $("#favLinks", {
+      "class" : defaults.favItemLink,
+      "text": params.title
+    }).appendTo(wrapper);
+  };
+  //https://medium.com/@elijahvalentinetroyabako/building-a-to-do-list-with-jquery-and-local-storage-f287bd95d4dd
